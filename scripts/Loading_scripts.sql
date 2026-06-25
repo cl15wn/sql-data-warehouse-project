697995fae72938a4dff73917ea95767a99a6f9a2
cl15wn <159258067+cl15wn@users.noreply.github.com>
2026-06-25 16:23:24 +0000

    Create Loading_scripts.sql

---
+++ scripts/Loading_scripts.sql
@@ -0,0 +1,93 @@
+
+
+
+-- LOAD SCRIPTS
+TRUNCATE TABLE bronze.crm_cust_info;
+
+BULK INSERT  bronze.crm_cust_info
+FROM 'C:\Users\ZXCV\Downloads\cour\DATA_BARAA\SQL\sql-data-warehouse-project-main\datasets\source_crm\cust_info.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK 
+);
+
+
+-- TRUNCATE TABLE bronze.crm_cust_info;  removes only data of the table
+
+
+
+SELECT COUNT(*) FROM bronze.crm_cust_info;
+
+
+TRUNCATE TABLE bronze.crm_prd_info;
+
+SET DATEFORMAT dmy;
+GO
+
+-- Your BULK INSERT statement here
+BULK INSERT bronze.crm_prd_info
+FROM 'C:\Users\ZXCV\Downloads\cour\DATA_BARAA\SQL\sql-data-warehouse-project-main\datasets\source_crm\prd_info.csv'
+WITH (
+    FIRSTROW = 2, -- If your header is line 1
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n'
+);
+
+
+
+
+
+TRUNCATE TABLE bronze.crm_sales_details;
+
+BULK INSERT  bronze.crm_sales_details
+FROM 'C:\Users\ZXCV\Downloads\cour\DATA_BARAA\SQL\sql-data-warehouse-project-main\datasets\source_crm\sales_details.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK 
+);
+
+
+
+
+
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+
+BULK INSERT  bronze.erp_loc_a101
+FROM 'C:\Users\ZXCV\Downloads\cour\DATA_BARAA\SQL\sql-data-warehouse-project-main\datasets\source_erp\LOC_A101.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK 
+);
+
+
+
+
+
+TRUNCATE TABLE bronze.erp_cust_az12;
+
+BULK INSERT  bronze.erp_cust_az12
+FROM 'C:\Users\ZXCV\Downloads\cour\DATA_BARAA\SQL\sql-data-warehouse-project-main\datasets\source_erp\CUST_AZ12.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK 
+);
+
+
+
+
+
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+BULK INSERT  bronze.erp_px_cat_g1v2
+FROM 'C:\Users\ZXCV\Downloads\cour\DATA_BARAA\SQL\sql-data-warehouse-project-main\datasets\source_erp\PX_CAT_G1V2.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK 
+);
