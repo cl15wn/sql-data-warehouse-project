1c1121770d94de19ec6d4bdc37cfee56e49f0b90
cl15wn <159258067+cl15wn@users.noreply.github.com>
2026-06-25 15:26:27 +0000

    Update DDL.sql

--- scripts/DDL.sql
+++ scripts/DDL.sql
@@ -1,109 +1,63 @@
-/*CAUTION : Creted multiple table with different Datatypes , remembered to remove non working table and keep the working one
 
 
 
+USE DataWarehouse
 
-*/
+GO
 
 CREATE TABLE bronze.crm_cust_info(
-    cst_id INT,
-    cst_key NVARCHAR(50),
-    cst_firstname NVARCHAR(50),
-    cst_lastname NVARCHAR(50),
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
     cst_material_status NVARCHAR(50),
-    cst_gndr NVARCHAR(50),
-    cst_create_date DATE
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
 );
 
 GO
 
--- TABLE 1: Your Original Strict Type Table (with decimal fixes)
-CREATE TABLE bronze.crm_sales_strict (
-    sls_ord_num  NVARCHAR(50),
-    sls_prd_key  NVARCHAR(50),
-    sls_cust_id  INT,
-    sls_order_dt DATE,
-    sls_ship_dt  DATE,
-    sls_due_dt   DATE,
-    sls_sales    DECIMAL(18,2), -- Changed from INT to capture cents
-    sls_quantity INT,
-    sls_price    DECIMAL(18,2)  -- Changed from INT to capture cents
+CREATE TABLE bronze.crm_prd_info(
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+
+
 );
 
-GO
 
--- TABLE 2: The Tolerant Text Table
-CREATE TABLE bronze.crm_sales_tolerant (
-    sls_ord_num  NVARCHAR(50),
-    sls_prd_key  NVARCHAR(50),
-    sls_cust_id  NVARCHAR(50),
-    sls_order_dt NVARCHAR(50),
-    sls_ship_dt  NVARCHAR(50),
-    sls_due_dt   NVARCHAR(50),
-    sls_sales    NVARCHAR(50),
-    sls_quantity NVARCHAR(50),
-    sls_price    NVARCHAR(50)
+CREATE TABLE bronze.crm_sales_details(
+    sls_ord_num     NVARCHAR(50),
+    sls_prd_key     NVARCHAR(50),
+    sls_cust_id     INT,
+    sls_order_dt    INT,
+    sls_ship_dt     INT,
+    sls_due_dt      INT,
+    sls_sales       INT,
+    sls_quantity    INT,
+    sls_price       INT
 );
 
-GO
-
--- TABLE 1: Your Original Strict Type Table (with decimal fixes)
-CREATE TABLE bronze.crm_prd_info_strict(
-    prd_id INT,
-    prd_key NVARCHAR(50),
-    prd_nm NVARCHAR(50),
-    prd_cost DECIMAL(18,2),
-    prd_line NVARCHAR(50),
-    prd_start_dt DATE,
-    prd_end_dt DATE
-   );
-
-GO
-
--- TABLE 2: The Tolerant Text Table
-CREATE TABLE bronze.crm_prd_info_tolerant(
-    prd_id NVARCHAR(50),
-    prd_key NVARCHAR(50),
-    prd_nm NVARCHAR(50),
-    prd_cost NVARCHAR(50),
-    prd_line NVARCHAR(50),
-    prd_start_dt NVARCHAR(50),
-    prd_end_dt NVARCHAR(50)
-   )
-
-   GO
- -- TABLE 1: Your Original Strict Type Table (with decimal fixes)
-
-CREATE TABLE bronze.srm_cust_az12_strict(
-        cust_cid INT,
-        cust_bdate DATE,
-        cust_gen NVARCHAR(50)
-        )
-
-    GO
--- TABLE 2: The Tolerant Text Table
-CREATE TABLE bronze.srm_cust_az12_tolerant(
-        cust_cid NVARCHAR(50),
-        cust_bdate NVARCHAR(50),
-        cust_gen NVARCHAR(50)
-        )
-    GO
-
-CREATE TABLE bronze.srm_loc_a101(
-    loc_cid NVARCHAR(50),
-    loc_country NVARCHAR(50)
-    )
-
-GO
 
 
-CREATE TABLE bronze.srm_px_cat_g1v2(
-    px_cat_id NVARCHAR(50),
-    px_cat_cat NVARCHAR(50),
-    px_cat_subcat NVARCHAR(50),
-    px_cat_maintenance NVARCHAR(50)
-    )
-GO
+CREATE TABLE bronze.erp_loc_a101(
+    cid     NVARCHAR(50),
+    cntry   NVARCHAR(50)
+);
 
+CREATE TABLE bronze.erp_cust_az12(
+    cid     NVARCHAR(50),
+    bdate   DATE,
+    gen     NVARCHAR(50)
+);
 
-
+CREATE TABLE bronze.erp_px_cat_g1v2(
+    id          NVARCHAR(50),
+    cat         NVARCHAR(50),
+    subcat      NVARCHAR(50),
+    maintenance NVARCHAR(50)
+);
